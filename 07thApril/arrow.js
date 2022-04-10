7d77b1330e017f1ff26d2589652d14e924e62a18
yashpandey95 <76428654+yashpandey95@users.noreply.github.com>
2022-04-10 12:43:59 +0200

    Arrow functions

--- 07thApril/arrow.js
+++ 07thApril/arrow.js
@@ -1,1 +1,15 @@
+"use strict";
+//arrow functions
 
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = "Juice with ${apples} apples and ${oranges} oranges.";
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
