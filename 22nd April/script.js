758656042c4c5ed166c143480eddcce635acd6a2
yashpandey95 <76428654+yashpandey95@users.noreply.github.com>
2022-04-22 16:49:14 +0200

    Destructuring Arrays

---
+++ 22nd April/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+const arr = [2, 3, 4];
+
+const [x, y, z] = arr;
+
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , second] = restaurant.categories;
+// console.log(first, second);
+
+// const temp = main;
+// main = second;
+// second = temp;
+// [second, temp];
+
+console.log(main, second);
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [3, 4]];
+
+const [varOne, , varTwo] = nested;
+const [varThree, varFour] = varTwo;
+console.log(varOne, varThree, varFour);
